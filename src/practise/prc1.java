2ff83618ec0dec3e337a5b2aa2ece1f8b85663a7
eyasir329 <eyasi329@gmail.com>
2023-05-13 23:21:50 +0600

    prc

--- src/practise/prc1.java
+++ src/practise/prc1.java
@@ -5,5 +5,8 @@ public class prc1 {
 		System.out.println("Hello World!");
 		System.out.println("My name is Eyasir Ahamed");
 		System.out.println("Sheikh Hasina University");
+		System.out.println("Govt. Ashek Mahmud College");
+		System.out.println("Jamalpur Zilla School");
+		System.out.println("Jamalpur");
 	}
 }
