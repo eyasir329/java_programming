5be215cac2d37bbbc4c8c78b26ac50ebd4a07596
eyasir329 <eyasi329@gmail.com>
2023-05-13 22:09:50 +0600

    prac

---
+++ src/practise/prc1.java
@@ -0,0 +1,9 @@
+package practise;
+
+public class prc1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Eyasir Ahamed");
+		System.out.println("Sheikh Hasina University");
+	}
+}
