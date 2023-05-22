16f52cc9962417d19adb0eaaadfe74192ff5362d
eyasir329 <eyasir329@gmail.com>
2023-05-22 21:57:08 +0600

    applet

---
+++ excercise/applet/test1.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class test1 extends Applet {
+    public void paint(Graphics g){
+        g.drawString("Wellcome", 300, 300);
+    }
+}
